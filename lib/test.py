df50651ad32361749fd8d13eae079b699def98da
Alexander Turenko <alexander.turenko@tarantool.org>
2017-03-17 15:20:25 +0300

    Removed TODO comment: it is not run of a test
    It was added in 4e7322c5 as I had thought it's run of specific type of
    test. It is not run of a test, so we don't need to pass
    'test_run_current_test' inside `exefile()`.

--- lib/test.py
+++ lib/test.py
@@ -141,9 +141,6 @@ class Test:
             if os.path.exists(self.skip_cond):
                 sys.stdout = FilteredStream(self.tmp_result)
                 stdout_fileno = sys.stdout.stream.fileno()
-                # XXX: Not sure crash reporting is adequate on these tests,
-                # maybe we need to pass 'test_run_current_test' to their
-                # non-default servers.
                 execfile(self.skip_cond, dict(locals(), **server.__dict__))
                 sys.stdout.close()
                 sys.stdout = save_stdout
