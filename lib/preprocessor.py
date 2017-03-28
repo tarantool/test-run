tarantool/test-run

commit 63ace6e3dc00eb3f543f202fca5388678227c06b

Fixed stdout mismatch at Ctrl+C and some races

lib/preprocessor.py
@@ -6,7 +6,6 @@ from collections import deque
 
 import yaml
 from gevent import socket
-from test import FilteredStream
 
 from lib.admin_connection import AdminAsyncConnection
 
@@ -337,11 +336,7 @@ class TestState(object):
         self.parse_preprocessor(string)
 
     def cleanup(self):
-        # SIGINT can somehow lead sys.stdout to be regular stdout here
-        # XXX: as a side effect of this: commands printed to a terminal,
-        #      tarantool server hangs in waiting of the commands
-        if isinstance(sys.stdout, FilteredStream):
-            sys.stdout.clear_all_filters()
+        sys.stdout.clear_all_filters()
         # don't stop the default server
         self.servers.pop('default')
         for k, v in self.servers.iteritems():

lib/inspector.py
@@ -67,6 +67,7 @@ class TarantoolInspector(StreamServer):
                 raise
             except Exception, e:
                 print('error', e)
+                # XXX: colorize to make printing the trace looks intentinal
                 import traceback
                 traceback.print_exc()
                 result = { "error": repr(e) }

lib/tarantool_server.py
@@ -95,7 +95,27 @@ class LuaTest(FuncTest):
             # join inspector handler
             self.inspector.sem.wait()
         # stop any servers created by the test, except the default one
-        ts.cleanup()  # XXX: put under finally block?
+        ts.cleanup()
+
+    def killall_servers(self, server, ts, crash_occured):
+        """ kill all servers and crash detectors before stream swap """
+        check_list = ts.servers.values() + [server, ]
+
+        # check that all servers stopped correctly
+        for server in check_list:
+            crash_occured = crash_occured or server.process.returncode not in (None, 0, -signal.SIGKILL, -signal.SIGTERM)
+
+        for server in check_list:
+            server.process.poll()
+
+            if crash_occured:
+                # kill all servers and crash detectors on crash
+                if server.process.returncode is None:
+                    server.process.kill()
+                gevent.kill(server.crash_detector)
+            elif server.process.returncode is not None:
+                # join crash detectors of stopped servers
+                save_join(server.crash_detector)
 
     def execute(self, server):
         server.current_test = self
@@ -117,26 +137,19 @@ class LuaTest(FuncTest):
         lua = TestRunGreenlet(self.exec_loop, ts)
         self.current_test_greenlet = lua
         lua.start()
-        crash_occured = save_join(lua, timeout=self.TIMEOUT)
+        crash_occured = True
+        try:
+            crash_occured = save_join(lua, timeout=self.TIMEOUT)
+            self.killall_servers(server, ts, crash_occured)
+        except KeyboardInterrupt:
+            # prevent tests greenlet from writing to the real stdout
+            lua.kill()
 
-        # kill all servers and crash detectors before stream swap
-        check_list = ts.servers.values() + [server, ]
-
-        # check that all servers stopped correctly
-        for server in check_list:
-            crash_occured = crash_occured or server.process.returncode not in (None, 0, -signal.SIGKILL, -signal.SIGTERM)
-
-        for server in check_list:
-            server.process.poll()
-
-            if crash_occured:
-                # kill all servers and crash detectors on crash
-                if server.process.returncode is None:
-                    server.process.kill()
-                gevent.kill(server.crash_detector)
-            elif server.process.returncode is not None:
-                # join crash detectors of stopped servers
-                save_join(server.crash_detector)
+            # XXX: should we only stop all servers and close all connections,
+            #      but don't make cleanup? killall_servers() doing that work,
+            #      but doesn't close connections.
+            ts.cleanup()
+            raise
 
 class PythonTest(FuncTest):
     def execute(self, server):
@@ -646,7 +659,10 @@ class TarantoolServer(Server):
             color_stdout('Stopping the server ...\n', schema='serv_text')
         # kill only if process is alive
         if self.process.returncode is None:
-            self.process.terminate()
+            try:
+                self.process.terminate()
+            except OSError:
+                pass
             if self.crash_detector is not None:
                 save_join(self.crash_detector)
             self.wait_stop()

lib/worker.py
@@ -122,11 +122,18 @@ class Worker:
 
         reproduce_dir = os.path.join(main_vardir, 'reproduce')
         if not os.path.isdir(reproduce_dir):
-            os.makedirs(reproduce_dir)
+            # try-except to prevent races btw workers
+            try:
+                os.makedirs(reproduce_dir)
+            except OSError:
+                pass
         self.tests_file = os.path.join(reproduce_dir, '%s.tests.txt' % self.name)
 
         color_stdout.queue_msg_wrapper = \
             lambda output, w=self: w.wrap_output(output)
+
+        self.last_task_done = True
+        self.last_task_id = -1
 
         try:
             self.server = suite.gen_server()
@@ -138,10 +145,21 @@ class Worker:
             color_stdout('Worker "%s" cannot start tarantool server; ignoring tasks...\n' \
                 % self.name, schema='error')
 
+    # TODO: What if KeyboardInterrupt raised inside task_queue.get() and 'stop
+    #       worker' marker readed from the queue, but not returned to us?
+    def task_get(self, task_queue):
+        self.last_task_done = False
+        self.last_task_id = task_queue.get()
+        return self.last_task_id
+
     @staticmethod
-    def task_done(task_queue):
-        if 'task_done' in task_queue.__dict__.keys():
+    def is_joinable(task_queue):
+        return 'task_done' in task_queue.__dict__.keys()
+
+    def task_done(self, task_queue):
+        if Worker.is_joinable(task_queue):
             task_queue.task_done()
+        self.last_task_done = True
 
     def find_task(self, task_id):
         for cur_task in self.suite.tests:
@@ -172,13 +190,13 @@ class Worker:
     def run_loop(self, task_queue, result_queue):
         """ called from 'run_all' """
         while True:
-            task_id = task_queue.get()
+            task_id = self.task_get(task_queue)
             # None is 'stop worker' marker
             if task_id is None:
                 color_stdout('Worker "%s" exhaust task queue; stopping the server...\n' \
                     % self.name, schema='test_var')
                 self.suite.stop_server(self.server, self.inspector)
-                Worker.task_done(task_queue)
+                self.task_done(task_queue)
                 break
             short_status = self.run_task(task_id)
             result_queue.put(self.wrap_result(task_id, short_status))
@@ -190,7 +208,7 @@ class Worker:
                 color_stdout('Worker "%s" got signal to terminate; stopping the server...\n' \
                     % self.name, schema='test_var')
                 raise VoluntaryStopException()
-            Worker.task_done(task_queue)
+            self.task_done(task_queue)
 
     def run_all(self, task_queue, result_queue):
         if not self.initialized:
@@ -205,17 +223,19 @@ class Worker:
         result_queue.put(self.done_marker())
 
     def stop(self, task_queue, result_queue):
-        # some task was in progress when the exception raised
-        Worker.task_done(task_queue)
-        # unblock task_queue is case it's JoinableQueue
+        if not self.last_task_done:
+            self.task_done(task_queue)
         self.flush_all_tasks(task_queue, result_queue)
         self.suite.stop_server(self.server, self.inspector, silent=True)
 
     def flush_all_tasks(self, task_queue, result_queue):
-        while True:
-            task_id = task_queue.get()
+        """ A queue flusing is necessary only for joinable queue (when runner
+            controlling workers with using join() on task queues), but for
+            unification in reporting 'not_run' status it make sense to leave it
+            enabled for any queue type.
+        """
+        # None is 'stop worker' marker
+        while self.last_task_id:
+            task_id = self.task_get(task_queue)
             result_queue.put(self.wrap_result(task_id, 'not_run'))
-            Worker.task_done(task_queue)
-            # None is 'stop worker' marker
-            if task_id is None:
-                break
+            self.task_done(task_queue)

test-run-parallel.py
@@ -3,9 +3,11 @@
 
 # TODOs:
 # * Fix current bad effects caused by hitting Ctrl+C:
-#   * Commands for tarantool can be written to a terminal.
-#   * Tarantool servers can hang at waiting the commands.
-# * SIGTERM non-default tarantools at Ctrl+C (as part of above).
+#   * Commands for tarantool can be written to a terminal (fixed?)
+#   * Tarantool servers can hang after Ctrl+C (its worker doesn't blocked):
+#     * default (case: app/??? 'app.lua')
+#     * non-default (okay?)
+#     * unknown (case: app-tap/tarantoolctl.test.lua 'good_script.lua') -- SIGTERM group?
 # * Save output for failed tests and give it at the end.
 # * Limit workers count by tests count at max.
 
@@ -415,6 +417,9 @@ def main():
     try:
         main_loop()
     except KeyboardInterrupt as e:
+        # TODO: SIGTERM all workers (or all processes in our group except
+        #       myself?), then sleep 0.1 sec, then SIGKILL processes that still
+        #       alive.
         color_stdout('\n[Main process] Caught keyboard interrupt;'
                      ' waiting for processes for doing its clean up\n',
                      schema='test_var')

commit 769cb14a6bcf2da8d805fd34f0a82d63c8f25b17

Allow to call TestState.cleanup() twice
I hit by the case when KeyboardInterrupt was raised from
LuaTest.exec_loop() after (or during) ts.cleanup() and then
LuaTest.execute() called ts.cleanup() 2nd time.

This commit fixed the regression caused by 63ace6e3.

lib/preprocessor.py
@@ -338,7 +338,7 @@ class TestState(object):
     def cleanup(self):
         sys.stdout.clear_all_filters()
         # don't stop the default server
-        self.servers.pop('default')
+        self.servers.pop('default', None)
         for k, v in self.servers.iteritems():
             v.stop(silent=True)
             v.cleanup()