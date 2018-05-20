tarantool/test-run

commit da0fc359b7cc4e0c91bc7b72c286b91baedb70fb

Return listen URIs from test_run::create_cluster()

lib/preprocessor.py
@@ -221,6 +221,7 @@ class TestState(object):
         if temp.rpl_master:
             setattr(nmsp, 'master', temp.rpl_master.iproto.port)
         setattr(self.environ, sname, nmsp)
+        return self.servers[sname].iproto.uri
 
     def server_deploy(self, ctype, sname, opts):
         self.servers[sname].install()
@@ -274,7 +275,7 @@ class TestState(object):
     def server(self, ctype, sname, opts):
         attr = 'server_%s' % ctype
         if hasattr(self, attr):
-            getattr(self, attr)(ctype, sname, opts)
+            return getattr(self, attr)(ctype, sname, opts)
         else:
             raise LuaPreprocessorException(
                 'Unknown command for server: %s' % ctype

test_run.lua
@@ -94,11 +94,16 @@ local create_cluster_cmd1 = 'create server %s with script="%s/%s.lua"'
 local create_cluster_cmd2 = 'start server %s with wait_load=False, wait=False'
 
 local function create_cluster(self, servers, test_suite)
+    local uris = {}
     test_suite = test_suite or 'replication'
+
     for _, name in ipairs(servers) do
-        self:cmd(create_cluster_cmd1:format(name, test_suite, name))
+        uris[#uris + 1] =
+            self:cmd(create_cluster_cmd1:format(name, test_suite, name))
         self:cmd(create_cluster_cmd2:format(name))
     end
+
+    return uris
 end
 
 local drop_cluster_cmd1 = 'stop server %s'

commit ee913036a0b24be1bdc802e338d5e0bb50fd4a46

Bring back default behaviour of test-run API
It is about 'create server' command and create_cluster() Lua function
from test_run.new() API.

The default behaviour was broken in da0fc359.

lib/preprocessor.py
@@ -221,7 +221,8 @@ class TestState(object):
         if temp.rpl_master:
             setattr(nmsp, 'master', temp.rpl_master.iproto.port)
         setattr(self.environ, sname, nmsp)
-        return self.servers[sname].iproto.uri
+        if 'return_listen_uri' in opts and opts['return_listen_uri'] == 'True':
+            return self.servers[sname].iproto.uri
 
     def server_deploy(self, ctype, sname, opts):
         self.servers[sname].install()

test_run.lua
@@ -91,19 +91,29 @@ end
 
 
 local create_cluster_cmd1 = 'create server %s with script="%s/%s.lua"'
+local create_cluster_cmd1_return_listen_uri =
+    'create server %s with script="%s/%s.lua", return_listen_uri=True'
 local create_cluster_cmd2 = 'start server %s with wait_load=False, wait=False'
 
-local function create_cluster(self, servers, test_suite)
-    local uris = {}
+local function create_cluster(self, servers, test_suite, opts)
+    local opts = opts or {}
     test_suite = test_suite or 'replication'
 
+    local uris = {}
+
     for _, name in ipairs(servers) do
-        uris[#uris + 1] =
+        if opts.return_listen_uri then
+            local cmd1 = create_cluster_cmd1_return_listen_uri
+            uris[#uris + 1] = self:cmd(cmd1:format(name, test_suite, name))
+        else
             self:cmd(create_cluster_cmd1:format(name, test_suite, name))
+        end
         self:cmd(create_cluster_cmd2:format(name))
     end
 
-    return uris
+    if opts.return_listen_uri then
+        return uris
+    end
 end
 
 local drop_cluster_cmd1 = 'stop server %s'