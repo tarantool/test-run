tarantool/test-run

commit 9d73ef1281c59177c7c23aae1371e71b4038e7f1

Handle non-default server crash
TarantoolStartError is subject to propagate to a joining greenlet,
consider `lib.inspector.gevent_propagate_exc()`.

lib/tarantool_server.py
@@ -50,6 +50,8 @@ def save_join(green_obj, timeout=None):
         green_obj.join(timeout=timeout)
     except GreenletExit:
         return True
+    except TarantoolStartError:
+        return True
     return False
 
 

commit a8931a87be384dd830bbfd00a42b0715bfa5d7a5

Support expected fail of non-default server
Usage:

```
test_run:cmd("start server <name> with crash_expected=True")
```

Also fixed non-default server crash reporting, regression after
9d73ef1281c59177c7c23aae1371e71b4038e7f1

Fixes #92.

lib/preprocessor.py
@@ -161,12 +161,28 @@ class TestState(object):
         args = []
         if 'args' in opts:
             args = opts['args'][1:-1].split(' ')
-        self.servers[sname].start(silent=True, rais=True, wait=wait, wait_load=wait_load, args=args)
-        self.connections[sname] = self.servers[sname].admin
+
+        crash_expected = 'crash_expected' in opts and \
+            opts['crash_expected'] == 'True'
+        crash_occured = False
         try:
-            self.connections[sname]('return true', silent=True)
-        except socket.error as e:
-            LuaPreprocessorException('Can\'t start server '+repr(sname))
+            if crash_expected:
+                # disable crash detector
+                self.servers[sname].crash_expected = True
+            self.servers[sname].start(silent=True, rais=True, wait=wait,
+                                      wait_load=wait_load, args=args)
+        except Exception as e:
+            crash_occured = True
+            if not (crash_expected and \
+                    e.__class__.__name__ == 'TarantoolStartError'):
+                raise
+        if not crash_occured:
+            self.connections[sname] = self.servers[sname].admin
+            try:
+                self.connections[sname]('return true', silent=True)
+            except socket.error as e:
+                LuaPreprocessorException('Can\'t start server '+repr(sname))
+        return not crash_occured
 
     def server_stop(self, ctype, sname, opts):
         color_log('\nDEBUG: TestState[%s].server_stop(%s, %s, %s)\n' % (

lib/tarantool_server.py
@@ -50,8 +50,8 @@ def save_join(green_obj, timeout=None):
         green_obj.join(timeout=timeout)
     except GreenletExit:
         return True
-    except TarantoolStartError:
-        return True
+    # We don't catch TarantoolStartError here, because it catched in start()
+    # and crash_detect() methods to report replica crash.
     return False
 
 
@@ -115,10 +115,12 @@ class LuaTest(FuncTest):
                 # kill all servers and crash detectors on crash
                 if server.process.returncode is None:
                     server.process.kill()
-                gevent.kill(server.crash_detector)
+                if server.crash_detector is not None:
+                    gevent.kill(server.crash_detector)
             elif server.process.returncode is not None:
                 # join crash detectors of stopped servers
-                save_join(server.crash_detector)
+                if server.crash_detector is not None:
+                    save_join(server.crash_detector)
 
     def execute(self, server):
         server.current_test = self
@@ -397,8 +399,7 @@ class TarantoolServer(Server):
         self.use_unix_sockets = ini['use_unix_sockets']
         self._start_against_running = ini['tarantool_port']
         self.crash_detector = None
-        # use this option with inspector
-        # to enable crashes in test
+        # use this option with inspector to enable crashes in test
         self.crash_enabled = False
 
         # set in from a test let test-run ignore server's crashes
@@ -571,8 +572,7 @@ class TarantoolServer(Server):
         self.crash_detector = TestRunGreenlet(self.crash_detect)
         self.crash_detector.info = "Crash detector: %s" % self.process
         self.crash_detector.start()
-        wait = wait
-        wait_load = wait_load
+
         if wait:
             try:
                 self.wait_until_started(wait_load)

lib/worker.py
@@ -269,7 +269,7 @@ class Worker:
             raise
         except Exception as e:
             color_stdout(
-                'Worker "%s" received the following error; stopping...\n'
+                '\nWorker "%s" received the following error; stopping...\n'
                 % self.name + traceback.format_exc() + '\n', schema='error')
             raise
         return short_status