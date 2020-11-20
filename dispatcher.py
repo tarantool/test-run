0c8859dcf7133413596fc37e653a47468efd0615
Sergey Bronnikov <sergeyb@tarantool.org>
2020-11-20 20:17:54 +0000

    python3: fix import of SimpleQueue
    Part of #20
    
    Co-authored-by: Alexander Turenko <alexander.turenko@tarantool.org>

--- dispatcher.py
+++ dispatcher.py
@@ -7,7 +7,12 @@ import functools
 import yaml
 
 import multiprocessing
-from multiprocessing.queues import SimpleQueue
+try:
+    # Python 3.7+
+    from multiprocessing import SimpleQueue
+except ImportError:
+    # Python 2
+    from multiprocessing.queues import SimpleQueue
 
 from lib import Options
 from lib.utils import set_fd_cloexec
