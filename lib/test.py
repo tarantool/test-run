1e7818f702e712e03c869bbdd585a127c20fc922
Sergey Bronnikov <sergeyb@tarantool.org>
2021-01-12 14:06:00 +0000

    python3: use byte representation of test output
    Output of test unit/json.test contains incorrect Unicode codes and we cannot
    decode output to UTF-8. To avoid a problem we use byte representation of
    test output.
    
    Part of #20
    
    Co-authored-by: Alexander Turenko <alexander.turenko@tarantool.org>

--- lib/test.py
+++ lib/test.py
@@ -11,13 +11,6 @@ import traceback
 from functools import partial
 from hashlib import md5
 
-try:
-    # Python 2
-    from StringIO import StringIO
-except ImportError:
-    # Python 3
-    from io import StringIO
-
 from . import Options
 from .colorer import color_stdout
 from .utils import non_empty_valgrind_logs
@@ -59,7 +52,7 @@ class FilteredStream:
         # always open the output stream in line-buffered mode,
         # to see partial results of a failed test
         #
-        self.stream = open(filename, "w+", 1)
+        self.stream = open(filename, "wb+")
         self.filters = []
         self.inspector = None
 
@@ -67,11 +60,10 @@ class FilteredStream:
         """Apply all filters, then write result to the undelrying stream.
         Do line-oriented filtering: the fragment doesn't have to represent
         just one line."""
-        if isinstance(fragment, (bytes, bytearray)):
-            fragment = fragment.decode('utf-8')
-        fragment_stream = StringIO(fragment)
+        if sys.version[0] == '3' and isinstance(fragment, str):
+            fragment = fragment.encode('utf-8')
         skipped = False
-        for line in fragment_stream:
+        for line in fragment.splitlines(True):
             original_len = len(line.strip())
             for pattern, replacement in self.filters:
                 line = re.sub(pattern, replacement, line)
@@ -83,6 +75,10 @@ class FilteredStream:
                 self.stream.write(line)
 
     def push_filter(self, pattern, replacement):
+        if sys.version[0] == '3' and isinstance(pattern, str):
+            pattern = pattern.encode('utf-8')
+        if sys.version[0] == '3' and isinstance(replacement, str):
+            replacement = replacement.encode('utf-8')
         self.filters.append([pattern, replacement])
 
     def pop_filter(self):
