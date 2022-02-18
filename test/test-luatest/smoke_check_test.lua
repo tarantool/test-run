b4af4790e9ae50f25219fcdfb62a056502a69f3c
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-02-18 12:54:10 +0300

    Refactor luatest test related stuff
    - Remove test/luatest_helpers.lua
    - Move `default_cfg`, `env_cfg`, `box_cfg` functions from
      test/luatest_helpers.lua to test/instances/default.lua
    - Move `instance_uri` function from test/luatest_helpers.lua
      to test/luatest_helpers/server.lua and rename it into
      `build_instance_uri`
    - Break down one test into four smaller ones

--- test/test-luatest/smoke_check_test.lua
+++ test/test-luatest/smoke_check_test.lua
@@ -1,10 +1,11 @@
 local t = require('luatest')
+
+local server = require('test.luatest_helpers.server')
+
 local g = t.group()
 
-local luatest_helpers = require('test.luatest_helpers')
-
 g.before_all = function()
-    g.server = luatest_helpers.Server:new({
+    g.server = server:new({
         alias = 'my_server',
         env = {MY_ENV_VAR = 'test_value'},
         box_cfg = {memtx_memory = 100 * 1024 ^ 2},
@@ -16,20 +17,22 @@ g.after_all = function()
     g.server:stop()
 end
 
-g.test_smoke = function()
-    -- The server is started and operable.
+g.test_server_is_started_and_operable = function()
     local res = g.server:eval('return 42')
     t.assert_equals(res, 42)
+end
 
-    -- The database is bootstrapped and accessible.
-    local res = g.server:eval('return box.info.status')
+g.test_database_is_bootstrapped_and_accessible = function()
+    local res = g.server:exec(function() return box.info.status end)
     t.assert_equals(res, 'running')
+end
 
-    -- The environment variable is passed.
-    local res = g.server:eval('return os.getenv("MY_ENV_VAR")')
+g.test_environment_variable_is_passed = function()
+    local res = g.server:exec(function() return os.getenv('MY_ENV_VAR') end)
     t.assert_equals(res, 'test_value')
+end
 
-    -- The box.cfg() values are passed as well.
-    local res = g.server:eval('return box.cfg.memtx_memory')
+g.test_box_cfg_values_are_passed = function()
+    local res = g.server:exec(function() return box.cfg.memtx_memory end)
     t.assert_equals(res, 100 * 1024 ^ 2)
 end
