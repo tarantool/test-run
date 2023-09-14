8ebb3aae542303cf38691e6a31bbae3c7dfd40d2
Oleg Chaplashkin <ochaplashkin@tarantool.org>
2023-09-14 14:29:02 +0400

    Adapt use luatest with new `--no-clean` option
    The `--no-clean` option was added after the luatest update [1]. Test-run
    uses luatest but independently performs the preparation (creation and
    cleaning of directories). Therefore this option was added to avoid
    incompatibility between the two tools.
    
    [1] tarantool/luatest@18859f6
    
    Part of tarantool/luatest#308

--- lib/luatest_server.py
+++ lib/luatest_server.py
@@ -34,10 +34,10 @@ class LuatestTest(Test):
     def execute(self, server):
         """Execute test by luatest command
 
-        Execute `luatest -c --verbose <name>_test.lua --output tap` command.
-        Disable capture mode. Provide a verbose output in the tap format.
-        Extend the command by `--pattern <pattern>` if the corresponding
-        option is provided.
+        Execute `luatest -c --no-clean --verbose <name>_test.lua --output tap`
+        command. Disable capture mode and deletion of the var directory.
+        Provide a verbose output in the tap format. Extend the command by
+        `--pattern <pattern>` if the corresponding option is provided.
         """
         server.current_test = self
         script = os.path.join(os.path.basename(server.testdir), self.name)
@@ -47,7 +47,7 @@ class LuatestTest(Test):
         # Add luatest as the script.
         command.extend([server.luatest])
         # Add luatest command-line options.
-        command.extend(['-c', '--verbose', script, '--output', 'tap'])
+        command.extend(['-c', '--no-clean', '--verbose', script, '--output', 'tap'])
         if Options().args.pattern:
             for p in Options().args.pattern:
                 command.extend(['--pattern', p])
