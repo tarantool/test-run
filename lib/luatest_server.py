tarantool/test-run

commit 3b0ccd0721e5fc772faeab2481f52723d5b5a69d

luatest: detox test searching code
It is hard to understand and hard to extend. This commit attempts to
arrange the method.

Points to highlight:

* Two old hacks are kept as is and got TODO comments. I'm a bit worrying
  about compatibility here and I would return here back later.
* `tests` and `exclude` patterns are obtained directly from the CLI
  options object, because I don't see a reason to pass them over the
  test suite object.
* `Server.exclude_tests` helper is not used anymore. A filtering by tags
  is included right into the test searching method.
* All the filtering conditions are expressed in a similar way, so more
  ones may be added in a future.

lib/luatest_server.py
@@ -14,6 +14,7 @@ from lib.server import Server
 from lib.tarantool_server import Test
 from lib.tarantool_server import TestExecutionError
 from lib.tarantool_server import TarantoolServer
+from lib.utils import find_tags
 
 
 def timeout_handler(process, test_timeout):
@@ -141,18 +142,46 @@ class LuatestServer(Server):
     def find_tests(test_suite, suite_path):
         """Looking for *_test.lua, which are can be executed by luatest."""
 
-        def patterned(test, patterns):
-            answer = []
-            for i in patterns:
-                if test.name.find(i) != -1:
-                    answer.append(test)
-            return answer
-
+        # TODO: Investigate why this old hack is needed and drop
+        # it if possible (move the assignment to test_suite.py).
+        #
+        # cdc70f94701f suggests that it is related to the out of
+        # source build.
         test_suite.ini['suite'] = suite_path
-        tests = glob.glob(os.path.join(suite_path, '*_test.lua'))
 
-        tests = Server.exclude_tests(tests, test_suite.args.exclude)
-        test_suite.tests = [LuatestTest(k, test_suite.args, test_suite.ini)
-                            for k in sorted(tests)]
-        test_suite.tests = sum([patterned(x, test_suite.args.tests)
-                                for x in test_suite.tests], [])
+        # A pattern here means just a substring to find in a test
+        # name.
+        include_patterns = Options().args.tests
+        exclude_patterns = Options().args.exclude
+
+        accepted_tags = Options().args.tags
+
+        tests = []
+        for test_name in glob.glob(os.path.join(suite_path, '*_test.lua')):
+            # If neither of the include patterns are substrings of
+            # the given test name, skip the test.
+            if not any(p in test_name for p in include_patterns):
+                continue
+
+            # If at least one of the exclude patterns is a
+            # substring of the given test name, skip the test.
+            if any(p in test_name for p in exclude_patterns):
+                continue
+
+            # If --tags <...> CLI option is provided...
+            if accepted_tags:
+                tags = find_tags(test_name)
+                # ...and the test has neither of the given tags,
+                # skip the test.
+                if not any(t in accepted_tags for t in tags):
+                    continue
+
+            # Add the test to the execution list otherwise.
+            tests.append(LuatestTest(test_name, test_suite.args, test_suite.ini))
+
+        tests.sort(key=lambda t: t.name)
+
+        # TODO: Don't modify a test suite object's field from
+        # another object directly. It is much better to just
+        # return a list of tests from this method.
+        test_suite.tests = tests

commit 49dff63c89aeac93307785eb6dcb491159991ed5

luatest: fix ability to run a test several times
It is a regression from commit 3b0ccd0721e5 ("luatest: detox test
searching code"), PR #433.

The ability to run a test several times, especially in parallel, is very
useful to reproduce and debug an unstable behavior in the test.

Fixes #437

lib/luatest_server.py
@@ -180,13 +180,26 @@ class LuatestServer(Server):
 
         tests = []
         for test_name in glob.glob(os.path.join(suite_path, '*_test.lua')):
-            # If neither of the include patterns are substrings of
-            # the given test name, skip the test.
-            if not any(p in test_name for p in include_patterns):
+            # Several include patterns may match the given
+            # test[^1].
+            #
+            # The primary usage of this behavior is to run a test
+            # many times in parallel to verify its stability or
+            # to debug an unstable behavior.
+            #
+            # Execute the test once for each of the matching
+            # patterns.
+            #
+            # [^1]: A pattern matches a test if the pattern is a
+            #       substring of the test name.
+            repeat = sum(1 for p in include_patterns if p in test_name)
+            # If neither of the include patterns matches the given
+            # test, skip the test.
+            if repeat == 0:
                 continue
 
-            # If at least one of the exclude patterns is a
-            # substring of the given test name, skip the test.
+            # If at least one of the exclude patterns matches the
+            # given test, skip the test.
             if any(p in test_name for p in exclude_patterns):
                 continue
 
@@ -211,13 +224,15 @@ class LuatestServer(Server):
                 prefix_len = len(os.path.commonprefix(test_cases))
 
                 for test_case in test_cases:
-                    tests.append(LuatestTest(test_name, test_suite.args, test_suite.ini,
-                                             params={"test_case": test_case},
-                                             conf_name=test_case[prefix_len:]))
+                    test_obj = LuatestTest(test_name, test_suite.args, test_suite.ini,
+                                           params={"test_case": test_case},
+                                           conf_name=test_case[prefix_len:])
+                    tests.extend([test_obj] * repeat)
             else:
                 # If the test has no 'parallel' tag, run all the
                 # test cases as one task.
-                tests.append(LuatestTest(test_name, test_suite.args, test_suite.ini))
+                test_obj = LuatestTest(test_name, test_suite.args, test_suite.ini)
+                tests.extend([test_obj] * repeat)
 
         tests.sort(key=lambda t: t.name)
 